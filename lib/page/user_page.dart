c19646d53f2ae38830d7f0db941005aef1c3db7c
DongLy <448189597@qq.com>
2020-12-18 16:50:30 +0800

    finished user_page ui

--- lib/page/user_page.dart
+++ lib/page/user_page.dart
@@ -16,18 +16,24 @@ class _UserPageState extends State<UserPage> {
             title: Text('我的'),
           ),
           body: ListView(
-            children: <Widget>[_getUserHeader()],
+            children: <Widget>[
+              _getUserHeader(),
+              _getMyOrders(),
+              _getMyPropety(),
+              _getMyCollection()
+            ],
           )),
     );
   }
 
+  // 头部 布局
   Widget _getUserHeader() {
     return Container(
       width: double.infinity,
       height: 140,
       decoration: BoxDecoration(
           gradient: const LinearGradient(
-              colors: [Color(0xFFE43B3A), Color(0xFF07157)])),
+              colors: [Color(0xFFE43B3A), Color(0xFF07158)])),
       child: Stack(
         children: <Widget>[
           // 顶部 布局 头像 用户名 (京享值 小白信用 标签
@@ -183,6 +189,293 @@ class _UserPageState extends State<UserPage> {
                   ],
                 ),
               ))
+        ],
+      ),
+    );
+  }
+
+  // 订单 布局
+  Widget _getMyOrders() {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.only(top: 20, bottom: 20),
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(bottom: 10),
+                child: Icon(
+                  Icons.payment,
+                  color: Color(0xFFDB9D58),
+                ),
+              ),
+              Text(
+                "代付款",
+                style: TextStyle(color: Colors.black54),
+              )
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(bottom: 10),
+                child: Icon(
+                  Icons.airport_shuttle,
+                  color: Color(0xFFDB9D58),
+                ),
+              ),
+              Text(
+                "代收货",
+                style: TextStyle(color: Colors.black54),
+              )
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(bottom: 10),
+                child: Icon(
+                  Icons.chat,
+                  color: Color(0xFFDB9D58),
+                ),
+              ),
+              Text(
+                "代评价",
+                style: TextStyle(color: Colors.black54),
+              )
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(bottom: 10),
+                child: Icon(
+                  Icons.monetization_on,
+                  color: Color(0xFFDB9D58),
+                ),
+              ),
+              Text(
+                "退款/售后",
+                style: TextStyle(color: Colors.black54),
+              )
+            ],
+          ),
+          Icon(
+            // todo
+            Icons.line_weight,
+            size: 1,
+            color: Colors.black,
+          ),
+          Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(bottom: 10),
+                child: Icon(
+                  Icons.featured_play_list,
+                  color: Color(0xFFE51E1F),
+                ),
+              ),
+              Text(
+                "全部订单",
+                style: TextStyle(color: Colors.black54),
+              ),
+              Text("查看发票",
+                  style: TextStyle(fontSize: 11, color: Colors.black12))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  // 用户 资产信息
+  Widget _getMyPropety() {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.only(top: 20, bottom: 20),
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Text("2",
+                      style: TextStyle(fontSize: 22, color: Color(0xFFE8272B))),
+                  Text("张",
+                      style: TextStyle(fontSize: 12, color: Color(0xFFE8272B))),
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                "京东券",
+                style: TextStyle(color: Colors.black54),
+              ),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Text("0",
+                      style: TextStyle(fontSize: 22, color: Color(0xFFE8272B))),
+                  Text("元",
+                      style: TextStyle(fontSize: 12, color: Color(0xFFE8272B))),
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                "账户余额",
+                style: TextStyle(color: Colors.black54),
+              ),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Text("40",
+                      style: TextStyle(fontSize: 22, color: Color(0xFFE8272B))),
+                  Text("个",
+                      style: TextStyle(fontSize: 12, color: Color(0xFFE8272B))),
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                "京豆",
+                style: TextStyle(color: Colors.black54),
+              ),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Text("0",
+                      style: TextStyle(fontSize: 22, color: Color(0xFFE8272B))),
+                  Text("元",
+                      style: TextStyle(fontSize: 12, color: Color(0xFFE8272B))),
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                "红包",
+                style: TextStyle(color: Colors.black54),
+              ),
+            ],
+          ),
+          Icon(
+            // todo
+            Icons.line_weight,
+            size: 1,
+            color: Colors.black,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(bottom: 10),
+                child: Icon(
+                  Icons.account_balance_wallet,
+                  color: Color(0xFFe51e1f),
+                ),
+              ),
+              Text(
+                "我的资产",
+                style: TextStyle(color: Colors.black54),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  // 收藏 布局
+  Widget _getMyCollection() {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.only(top: 20, bottom: 20),
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Text(
+                '10',
+                style: TextStyle(fontSize: 22),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                "商品收藏",
+                style: TextStyle(color: Colors.black54),
+              )
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                '2',
+                style: TextStyle(fontSize: 22),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                "店铺收藏",
+                style: TextStyle(color: Colors.black54),
+              )
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                '10',
+                style: TextStyle(fontSize: 22),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                "搭配收藏",
+                style: TextStyle(color: Colors.black54),
+              )
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                '0',
+                style: TextStyle(fontSize: 22),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                "我的足迹",
+                style: TextStyle(color: Colors.black54),
+              )
+            ],
+          ),
         ],
       ),
     );
